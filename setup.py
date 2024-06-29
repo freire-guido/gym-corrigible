99aad6ecac0c3f92c1f2c16a7ea55413c9fb3abc
freire-guido <14301656+freire-guido@users.noreply.github.com>
2024-06-29 12:34:48 -0300

    cleanup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="gym_corrigible",
     version="0.0.1",
-    install_requires=["gymnasium==0.26.0", "pygame==2.1.0", "minigrid==2.3.1"],
+    install_requires=["gymnasium==0.29.1", "pygame==2.6.0", "minigrid==2.3.1"],
 )
